3a2ed1b7636849c8365a6e9ba83c836514043214
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-10 15:28:47 +0300

    add structure to potential , followup and dashboard modules

---
+++ src/app/modules/followup/followup.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FollowupRoutingModule } from './followup-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FollowupRoutingModule
+  ]
+})
+export class FollowupModule { }
---
+++ src/app/modules/potential/potential.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PotentialRoutingModule } from './potential-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PotentialRoutingModule
+  ]
+})
+export class PotentialModule { }
