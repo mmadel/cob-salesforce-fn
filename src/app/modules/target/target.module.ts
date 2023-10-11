c2c05a6855d63e386979031e0cf8310862937889
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-11 12:44:48 +0300

    create target module

---
+++ src/app/modules/target/target.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TargetRoutingModule } from './target-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TargetRoutingModule
+  ]
+})
+export class TargetModule { }
