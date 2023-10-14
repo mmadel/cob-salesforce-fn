4e46e6d131265755fd0d65b8c07156c84e77b545
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-14 18:52:35 +0300

    websocket to get potential and followup numbers

--- src/app/modules/dashboard/components/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { merge } from 'rxjs';
 import { DashboardCounters } from '../models/dashboar.counters';
 import { DashboardService } from '../services/dashboard.service';
+import * as Stomp from 'stompjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,7 +16,13 @@ export class DashboardComponent implements OnInit {
   followupDoctorsCounter: string
   userAchievement: string;
   userFirstTimeVisitTarget: string
-  userFirstTimeVisitAchievement:string;
+  userFirstTimeVisitAchievement: string;
+
+  showConversation: boolean = false;
+  ws: any;
+  name: string;
+  disabled: boolean;
+  clinicId: string = '1';
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit(): void {
@@ -27,6 +34,47 @@ export class DashboardComponent implements OnInit {
         this.userFirstTimeVisitTarget = response.userFirstTimeVisitTarget + ''
         this.userFirstTimeVisitAchievement = response.userFirstTimeVisitAchievement + ''
       })
+    this.connect();
+  }
+  connect() {
+    let socket = new WebSocket("ws://localhost:8080/salesforce-service/api/counters");
+    this.ws = Stomp.over(socket);
+
+    this.ws.connect({}, (frame: any) => {
+      this.ws.subscribe("/errors", (message: any) => {
+        alert("Error " + message.body);
+      });
+      this.ws.subscribe("/topic/potential", (message: any) => {
+        this.potentialDoctorsCounter = message.body;
+        console.log(message)
+      });
+      this.ws.subscribe("/topic/followup", (message: any) => {
+        var payload = message.body.split("_");
+        console.log(payload[0] +' ' + payload[1])
+        if (this.clinicId === payload[0])
+          this.followupDoctorsCounter = payload[1];
+      });
+      this.disabled = true;
+    }, (error: any) => {
+      alert("STOMP error " + error);
+    });
+  }
+  disconnect() {
+    if (this.ws != null) {
+      this.ws.ws.close();
+    }
+    this.setConnected(false);
+    console.log("Disconnected");
   }
 
+  setConnected(connected: any) {
+    this.disabled = connected;
+    this.showConversation = connected;
+  }
+  sendName() {
+    let data = JSON.stringify({
+      'name': this.name
+    })
+    this.ws.send("/app/message", {}, data);
+  }
 }
