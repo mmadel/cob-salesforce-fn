ed78486d656c62a5a392743cd73a42596a5db2d5
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-16 17:56:21 +0300

    link first visit to seleced clinic and user

--- src/app/modules/potential/components/first.visit.create/first-visit-create.component.ts
+++ src/app/modules/potential/components/first.visit.create/first-visit-create.component.ts
@@ -3,7 +3,10 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
 import { ToastrService } from 'ngx-toastr';
+import { combineLatest, filter, from, switchMap, tap } from 'rxjs';
+import { ClinicService } from 'src/app/modules/administration/services/clinic/clinic.service';
 import { Followup } from 'src/app/modules/followup/models/followup.model';
+import { KcAuthService } from 'src/app/modules/security/service/kc/kc-auth.service';
 import { ContactPosition } from '../../enums/contact.position';
 import { Impression } from '../../enums/followup.impression';
 import { FollowupCreationService } from '../../services/followup-creation.service';
@@ -18,12 +21,19 @@ export class FirstVisitCreateComponent implements OnInit {
   firstVisit: Followup;
   impressions = Impression;
   contactPositions = ContactPosition;
+  userUUId: string;
+  clinicId: string;
   @ViewChild('firstVisitForm') firstVisitForm: NgForm;
   constructor(private followupCreationService: FollowupCreationService
     , private toastr: ToastrService
-    , private router: Router) { }
+    , private router: Router
+    , private clinicService: ClinicService
+    , private kcAuthService: KcAuthService) { }
 
   ngOnInit(): void {
+    this.clinicService.selectedClinic$.subscribe((re) => {
+      console.log('################### ' + re)
+    })
     this.firstVisit = {
       dateOfVisit: 0,
       impression: '',
@@ -42,17 +52,23 @@ export class FirstVisitCreateComponent implements OnInit {
   }
   create() {
     if (this.firstVisitForm.valid) {
-      this.populateModel();
-      this.followupCreationService.createFirstVisit(this.firstVisit).subscribe((response) => {
-        this.toastr.success('First Visit Created successfully');
-        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
-          this.router.navigate(['administrator/potential/list']));
-      },
-        (error) => {
-          this.toastr.error(error.error.message, 'Error while submit followup');
-        });
-    } else {
-      this.errorMessage = 'Please enter valid data';
+      combineLatest([from(this.kcAuthService.loadUserProfile()), this.clinicService.selectedClinic$])
+        .pipe(
+          filter((result) => result[1] !== null),
+          tap((result) => {
+            this.userUUId = result[0].id!;
+            this.clinicId = result[1] !== null ? result[1].toString() : ""
+            this.populateModel();
+          }),
+          switchMap(result => this.followupCreationService.createFirstVisit(this.firstVisit)))
+        .subscribe((response) => {
+          this.toastr.success('First Visit Created successfully');
+          this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
+            this.router.navigate(['administrator/target/first']));
+        },
+          (error) => {
+            this.toastr.error(error.error.message, 'Error while submit followup');
+          });
     }
   }
   calculateDates() {
@@ -61,12 +77,11 @@ export class FirstVisitCreateComponent implements OnInit {
     this.firstVisit.nextFollowupDate_str = moment(this.firstVisit.dateOfVisit_str).add(1, 'weeks').format("MM/DD/YYYY");
   }
   populateModel() {
-    this.firstVisit.doctor!.clinicId = "1"
+    this.firstVisit.doctor!.clinicId = this.clinicId
     this.firstVisit.dateOfVisit = Number(moment(this.firstVisit.dateOfVisit_str).format("x"))
     this.firstVisit.nextFollowupDate = Number(moment(this.firstVisit.nextFollowupDate_str).format("x"))
     this.firstVisit.user = {
-      id: 1,
-      uuid: 'e066f671-c714-40da-af03-b9c3252eb252'
+      uuid: this.userUUId
     }
   }
 }
