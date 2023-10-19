6a3a743a38e3ec87beef9c7a5a169a4036f6a659
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-19 11:28:41 +0300

    add wsdomain to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ import keycloakConfigProd from "./keycloak.config.prod";
 export const environment = {
   production: true,
   baseURL:"/salesforce-service/api/",
+  wsdomain:"sales-service-25bf3634a2d0.herokuapp.com",
   keycloak: keycloakConfigProd,
 };
