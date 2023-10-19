0abcd87c95e5e7d936bb7582a07cdcbaee49c0f1
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-19 18:02:06 +0300

    get emitted clinic id if first time config component

--- src/app/modules/administration/components/first.time.target/first-time-target.component.ts
+++ src/app/modules/administration/components/first.time.target/first-time-target.component.ts
@@ -3,16 +3,16 @@ import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { User } from '../../model/user';
 import { UserTarget } from '../../model/user.target';
+import { ClinicService } from '../../services/clinic/clinic.service';
 import { UserTargetService } from '../../services/target.service/user-target.service';
 import { UserService } from '../../services/user.service';
-
+import { combineLatest, filter, merge, switchMap, tap } from 'rxjs';
 @Component({
   selector: 'app-first-time-target',
   templateUrl: './first-time-target.component.html',
   styleUrls: ['./first-time-target.component.css']
 })
 export class FirstTimeTargetComponent implements OnInit {
-  clinicId: string = "1";
   errorMessage!: string | null;
   @ViewChild('firstTimeConfigForm') firstTimeConfigForm: NgForm;
   users: User[];
@@ -21,27 +21,35 @@ export class FirstTimeTargetComponent implements OnInit {
   submitted = false;
   constructor(private userService: UserService
     , private userTargetService: UserTargetService
-    , private toastr: ToastrService) { }
+    , private toastr: ToastrService
+    , private clinicService: ClinicService) { }
 
   ngOnInit(): void {
-    this.userService.getUserByClinic(this.clinicId).subscribe((response) => {
-      this.users = response;
-    })
+    merge(this.clinicService.selectedClinic$)
+      .pipe(
+        filter((selectedClinic) => selectedClinic !== null),
+        switchMap(selectedClinic => this.userService.getUserByClinic(selectedClinic?.toString()!))
+      ).subscribe((response) => {
+        this.users = response;
+      }, (error) => {
+        console.log(JSON.stringify(error))
+      })
+      ;
   }
   create() {
     this.submitted = true;
     if (this.firstTimeConfigForm.valid) {
-      var userTarget:UserTarget={
-        firstTime : this.firstVisitTarget,
-        userUUID : this.userUUID
+      var userTarget: UserTarget = {
+        firstTime: this.firstVisitTarget,
+        userUUID: this.userUUID
       }
-      this.userTargetService.update(userTarget).subscribe((response)=>{
+      this.userTargetService.update(userTarget).subscribe((response) => {
         this.toastr.success('First Visit configuration created successfully');
         this.submitted = false;
         this.firstTimeConfigForm.reset();
       })
     }
-    
+
   }
   resetError() {
     this.submitted = false;
